7edd74f93d30a2abe53f62a689ebb4d815141b67
selbekk <kristofer.selbekk@bekk.no>
2015-05-29 21:36:56 +0200

    yo

--- test.js
+++ test.js
@@ -2,4 +2,4 @@ var Logger = require('./build');
 var log = new Logger();
 
 var name = 'Kris';
-log.info('hello {}', name, 'second arg', 'third arg');
+log.info('hello {} {} {}', name, 'second arg', 'third arg');
